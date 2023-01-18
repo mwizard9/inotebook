ecd49c3ebb1247be4cbb5a52f7b0d0aeca5f6c8b
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-18 15:27:28 +0545

    testing databse by thunderclient

--- Backend/db.js
+++ Backend/db.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://Mwizard:maxweLL11!!@cluster0.odmicfm.mongodb.net/Notebook?retryWrites=true&w=majority"
+
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to mongo successfully");
+    })
+}
+
+module.exports = connectToMongo;
