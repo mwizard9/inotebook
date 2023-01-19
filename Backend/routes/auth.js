4620c7349affb361b3b4608bdfa81e5ce1b05c41
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-19 16:01:15 +0545

    sending error msg validation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,15 +5,24 @@ const { body, validationResult } = require('express-validator');
 
 //Create a User using : POST "/api/auth/". Doesn't require Auth
 router.post('/',[
-   body('email').isEmail(),
-   body('name').isLength({ min: 3 }),
-   body('password').isLength({ min: 5 }),
+   body('email','Enter a valid name').isEmail(),
+   body('name','enter a valid email').isLength({ min: 3 }),
+   body('password','Password must be at least five charecters').isLength({ min: 5 }),
 ],
 (req,res)=>{
    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-})
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email
+    }).then(user => res.json(user))
+    .catch(err=> {console.log(err)
+   res.json({error: 'Please enter a unique email ID',message:err.message})})
+  },
+);
+    
 
 module.exports = router
