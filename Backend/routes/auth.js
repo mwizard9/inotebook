ac8a33bff45d0f9592298016988a84d978535f2b
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-20 17:31:25 +0545

    using try catch

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -9,29 +9,34 @@ router.post('/createUser', [
   body('name', 'enter a valid email').isLength({ min: 3 }),
   body('password', 'Password must be at least five charecters').isLength({ min: 5 }),
 ],
-  async(req, res) => {
+  async (req, res) => {
     //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     //check weather the user with this email exists already
-    let user = await User.findOne({email: req.body.email});
-    console.log(user)
-    if (user) {
-      return res.status(400).json({ error: "sorry a user with this email already exists" });
+    try {
+
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "sorry a user with this email already exists" });
+      }
+
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+      })
+      res.json({ user });
+
+      //.then(user => res.json(user))
+      //.catch(err=> {console.log(err)
+      //res.json({error: 'Please enter a unique email ID',message:err.message})})
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occure")
     }
-
-    user = await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email
-    })
-    res.json({"Nice": "nice"});
-
-    //.then(user => res.json(user))
-    //.catch(err=> {console.log(err)
-    //res.json({error: 'Please enter a unique email ID',message:err.message})})
   },
 );
 
