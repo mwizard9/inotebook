e7dd6b184ef0c22e4c76cd6a7ce5151c9134cc95
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-24 17:11:10 +0545

    update Note

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -45,4 +45,25 @@ router.post('/addnotes', fetchuser, [
     }
 
 })
+
+// ROUTE 3:update the notes, login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    //Create a newnote object
+    const newNote = {};
+    if (title){newNote.title = title};
+    if (description){newNote.description = description};
+    if (tag){newNote.tag = tag};
+
+    //find the note to be updatated and updateit
+    let note = await Notes.findById(req.params.id);
+    if (!note) {return res.status(404).send("Not Found")}
+
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json({note});
+
+})
 module.exports = router
