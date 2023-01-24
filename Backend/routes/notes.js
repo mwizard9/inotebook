6b16515f3e84a83b08708d59cc07c944cabbd6a6
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-24 17:59:11 +0545

    delete the note

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -66,4 +66,23 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
     res.json({note});
 
 })
+
+// ROUTE 3:delete the notes, login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+   
+
+    //find the note to be updatated and delete it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {return res.status(404).send("Not Found")}
+    
+    //Allow deletion only if user owns this Note
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndDelete(req.params.id)
+    res.json({"Sucess":"Note has been Deleted",note: note});
+})
+
+
 module.exports = router
