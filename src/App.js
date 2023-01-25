45fffdac7be402ab71ef4f769653c6517bc2c6be
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-25 14:34:07 +0545

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import{
+  BrowserRouter as Router,Routes,Route,Link
+} from "react-router-dom";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar/>
       <h1>This is INotebook</h1>
-    </div>
+      
+    </>
   );
 }
 
