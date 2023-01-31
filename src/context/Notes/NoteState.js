4437e17df589a52a87ec4e9a967fedaa4451fb61
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-31 16:06:39 +0545

    featch API for featching all note

--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -5,45 +5,23 @@ import NoteContext from "./NoteContext";
 const NoteState = (props) => {
   const host = "http://localhost:5000"
     const notesInitial = 
-        [
-            {
-              "_id": "63ce5813f09ae0e26234c474",
-              "user": "63ce57eef09ae0e26234c470",
-              "title": "any Title",
-              "description": "this is my note",
-              "tag": "personal",
-              "date": "2023-01-23T09:49:07.513Z",
-              "__v": 0
-            },
-            {
-              "_id": "63ce581ff09ae0e26234c476",
-              "user": "63ce57eef09ae0e26234c470",
-              "title": "my Title",
-              "description": "this is my note",
-              "tag": "personal",
-              "date": "2023-01-23T09:49:19.273Z",
-              "__v": 0
-            },
-            {
-              "_id": "63ce5820f09ae0e26234c478",
-              "user": "63ce57eef09ae0e26234c470",
-              "title": "my Title",
-              "description": "this is my note",
-              "tag": "personal",
-              "date": "2023-01-23T09:49:20.615Z",
-              "__v": 0
-            },
-            {
-              "_id": "63cfb4efe9f7b3d396bdafd1",
-              "user": "63ce57eef09ae0e26234c470",
-              "title": "my Title",
-              "description": "this is my note",
-              "tag": "personal",
-              "date": "2023-01-24T10:37:35.717Z",
-              "__v": 0
-            }
-          ]
+        [ ]
           const [notes,setNotes] =useState(notesInitial)
+
+          //Get all Notes
+          const getNotes = async () => {
+            // API call
+            const response = await fetch(`${host}/api/notes/featchallnotes`, {
+              method: 'POST', 
+              headers: {
+                'Content-Type': 'application/json',
+                'auth-token' : 'eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNjZTU3ZWVmMDlhZTBlMjYyMzRjNDcwIn0sImlhdCI6MTY3NDQ2NzMxMH0.2rkB8eMa1UIpGb1mVTpA7t1KVAsQpg1XRgDywBTcMBQ'
+                
+              },
+          });
+          const json = await response.json()
+          console.log(json)
+        }
           
           
           //Add a Note
