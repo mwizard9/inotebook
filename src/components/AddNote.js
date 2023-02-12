756d35d2a8c8a24ef5ce0f3ee2484b51c8a289f1
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-12 09:31:42 +0545

    fix in add note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,10 +5,11 @@ const AddNote = () => {
     const context = useContext(noteContext)
     const {addNote} = context;
 
-    const[notes,setNote] = useState({title:"",description:"",tag:"default"});
+    const[notes,setNote] = useState({title:"",description:"",tag:""});
     const handleClick = (e) => {
         e.preventDefault();
         addNote(notes.title, notes.description, notes.tag);
+        setNote({ title: "", description: "", tag: ""})
 
     }
     const onChange = (e)=>{
@@ -22,16 +23,16 @@ const AddNote = () => {
         <form className='my-3 mx-3'>
           <div className="mb-3">
             <label htmlFor="title" className="form-label">Title</label>
-            <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" onChange={onChange} />
+            <input type="text" className="form-control" id="title" name='title' value={notes.title} aria-describedby="emailHelp" onChange={onChange} />
      
           </div>
           <div className="mb-3">
             <label htmlFor="description" className="form-label">Description</label>
-            <input type="text" className="form-control" id="description" name='description' onChange={onChange} />
+            <input type="text" className="form-control" id="description" name='description' value={notes.description} onChange={onChange} />
           </div>
           <div className="mb-3">
             <label htmlFor="tag" className="form-label">Tag</label>
-            <input type="text" className="form-control" id="tag" name='tag' onChange={onChange} />
+            <input type="text" className="form-control" id="tag" value={notes.tag} name='tag' onChange={onChange} />
           </div>
           <button onClick={handleClick} type="submit" className="btn btn-primary">Add Note</button>
         </form>
