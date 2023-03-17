ac1675f435cba50bb7d6ee9f83f28e57606e1b5f
Manish Himalaya <manishhimalaya64@gmail.com>
2023-03-17 17:58:30 +0545

    adding show alert function

--- src/App.js
+++ src/App.js
@@ -10,9 +10,20 @@ import NoteState from './context/Notes/NoteState';
 import Alert from './components/Alert';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { useState } from 'react';
 
 
 function App() {
+  const [alert,setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1500);
+  }
   return (
     <>
     <NoteState>
